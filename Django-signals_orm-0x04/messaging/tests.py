c6faef6784f941c4599008f77bff46aa468faa01
Biruk Abebe <159183117+BrukAbebe@users.noreply.github.com>
2025-06-12 23:37:35 +0300

    Create tests.py

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification, MessageHistory
+
+class MessagingTests(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='user1', password='testpass123')
+        self.user2 = User.objects.create_user(username='user2', password='testpass123')
+
+    def test_message_notification(self):
+        message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            content="Test message"
+        )
+        notification = Notification.objects.filter(user=self.user2, message=message).first()
+        self.assertIsNotNone(notification)
+        self.assertFalse(notification.read)
+
+    def test_message_edit_history(self):
+        message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            content="Original content"
+        )
+        message.content = "Edited content"
+        message.save()
+        history = MessageHistory.objects.filter(message=message).first()
+        self.assertIsNotNone(history)
+        self.assertEqual(history.old_content, "Original content")
+        self.assertTrue(message.edited)
+
+    def test_user_deletion(self):
+        message = Message.objects.create(
+            sender=self.user1,
+            receiver=self.user2,
+            content="Test message"
+        )
+        self.user1.delete()
+        self.assertEqual(Message.objects.count(), 0)
+        self.assertEqual(Notification.objects.count(), 0)
