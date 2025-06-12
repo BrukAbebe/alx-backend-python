36209c3c85da0978558deadd7381bfb9c2b64572
Biruk Abebe <159183117+BrukAbebe@users.noreply.github.com>
2025-06-13 00:18:38 +0300

    Update models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,20 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UnreadMessagesManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(read=False)
-
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
-    read = models.BooleanField(default=False)
-    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='replies')
-    objects = models.Manager()  # Default manager
-    unread = UnreadMessagesManager()  # Custom manager
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
 
     class Meta:
         ordering = ['timestamp']
